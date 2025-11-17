a6d6a075d3d56c1585c2f529dc0030ff9aed0c98
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2025-11-17 12:49:01 +0400

    Ajout de commentaires explicatifs dans les templates Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,25 @@
 <?php
 
+// Déclare le namespace pour organiser le code et éviter les conflits de noms
 namespace App\Controller;
 
+// Importe la classe de base des contrôleurs Symfony
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// Importe la classe pour gérer les réponses HTTP
 use Symfony\Component\HttpFoundation\Response;
+// Importe l'attribut Route pour définir les routes directement sur les méthodes
 use Symfony\Component\Routing\Attribute\Route;
 
+// Déclare la classe HomeController qui hérite des fonctionnalités de AbstractController
 class HomeController extends AbstractController
 {
+    // Attribut PHP : définit la route /home et le nom de la route "app_home"
     #[Route('/home', name: 'app_home')]
+    // Méthode appelée quand la route /home est visitée
     public function index(): Response
     {
+        // Génère une réponse HTML en rendant le template Twig 'home/index.html.twig'
+        // et en lui passant la variable 'controller_name'
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
