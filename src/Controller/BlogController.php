debbc27b5079ffe0b82e206e9043c5181c922c08
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2025-11-17 15:07:07 +0400

    Affichage des articles du blog par slug

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,11 +10,28 @@ use Symfony\Component\Routing\Attribute\Route;
 // Contrôleur pour la page "Blog"
 final class BlogController extends AbstractController
 {
-    // Route pour /blog
+    // Route pour /blog (liste des articles)
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-        // Rend le template Twig avec une variable
         return $this->render('blog/index.html.twig');
+    }
+
+    // Route pour afficher un article par son slug
+    #[Route('/blog/{slug}', name: 'app_blog_show', requirements: ['slug' => '[a-z0-9-]+'])]
+    public function show(string $slug): Response
+    {
+        // Ici, on afficherait l'article correspondant au slug
+        return $this->render('blog/show.html.twig', [
+            'article_slug' => $slug,
+        ]);
+    }
+
+    // Route pour la création d'un article
+    #[Route('/blog/new', name: 'app_blog_new')]
+    public function new(): Response
+    {
+        // Ici, on afficherait un formulaire de création d'article
+        return $this->render('blog/new.html.twig');
     }
 }
