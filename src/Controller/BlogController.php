eda2921c68b113301ccb57e2befe75e64d472418
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2025-11-17 15:57:47 +0400

    Bouton Publier en noir sur le formulaire d'article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,15 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 // Contrôleur pour la page "Blog"
 final class BlogController extends AbstractController
 {
-        // Route pour filtrer les articles par catégorie
-        #[Route('/blog/category/{category}', name: 'app_blog_by_category', requirements: ['category' => '[a-zA-Z0-9_-]+'])]
-        public function byCategory(string $category): Response
-        {
-            // Ici, on afficherait la liste des articles de la catégorie donnée
-            return $this->render('blog/category.html.twig', [
-                'category' => $category,
-            ]);
-        }
+    // Route pour filtrer les articles par catégorie
+    #[Route('/blog/category/{category}', name: 'app_blog_by_category', requirements: ['category' => '[a-zA-Z0-9_-]+'])]
+    public function byCategory(string $category): Response
+    {
+        // Ici, on afficherait la liste des articles de la catégorie donnée
+        return $this->render('blog/category.html.twig', [
+            'category' => $category,
+        ]);
+    }
     // Route pour /blog (liste des articles)
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
