d3b932c4f21191fcf56264fbe7daf33ca5ed413e
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2025-11-17 15:31:57 +0400

    Ajout du filtrage par catégorie dans le menu Blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,11 +10,28 @@ use Symfony\Component\Routing\Attribute\Route;
 // Contrôleur pour la page "Blog"
 final class BlogController extends AbstractController
 {
+        // Route pour filtrer les articles par catégorie
+        #[Route('/blog/category/{category}', name: 'app_blog_by_category', requirements: ['category' => '[a-zA-Z0-9_-]+'])]
+        public function byCategory(string $category): Response
+        {
+            // Ici, on afficherait la liste des articles de la catégorie donnée
+            return $this->render('blog/category.html.twig', [
+                'category' => $category,
+            ]);
+        }
     // Route pour /blog (liste des articles)
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig');
+    }
+
+    // Route pour la création d'un article
+    #[Route('/blog/new', name: 'app_blog_new')]
+    public function new(): Response
+    {
+        // Ici, on afficherait un formulaire de création d'article
+        return $this->render('blog/new.html.twig');
     }
 
     // Route pour afficher un article par son slug
@@ -25,13 +42,5 @@ final class BlogController extends AbstractController
         return $this->render('blog/show.html.twig', [
             'article_slug' => $slug,
         ]);
-    }
-
-    // Route pour la création d'un article
-    #[Route('/blog/new', name: 'app_blog_new')]
-    public function new(): Response
-    {
-        // Ici, on afficherait un formulaire de création d'article
-        return $this->render('blog/new.html.twig');
     }
 }
